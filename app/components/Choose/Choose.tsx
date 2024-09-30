b27d35ca21cb4293cd401b64f075aac0525ac084
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-09-30 19:16:41 +0200

    Feat: Choose tablet

--- app/components/Choose/Choose.tsx
+++ app/components/Choose/Choose.tsx
@@ -6,14 +6,16 @@ import ChooseItem from './ChooseItem';
 
 export default function Choose() {
   return (
-    <article className="relative mb-[120px] h-[1499px] w-[327px]">
-      <div className="aboslute bg-chooseBg right-0 top-0 -z-10 h-[902px] rounded-[10px]"></div>
-      <div className="absolute left-2/4 top-[64px] z-10 flex w-[279px] translate-x-[-50%] flex-col gap-[64px]">
+    <article className="relative mb-[120px] h-[1499px] w-[327px] md:mb-[144px] md:h-[863px] md:w-[688px]">
+      <div className="aboslute right-0 top-0 -z-10 h-[902px] rounded-[10px] bg-chooseBg md:h-[573px]"></div>
+      <div className="absolute left-2/4 top-[64px] z-10 flex w-[279px] translate-x-[-50%] flex-col gap-[64px] md:top-[56px] md:w-[540px]">
         <div className="flex flex-col gap-[24px] text-center text-cream">
-          <h2 className={`${fraunces.className} text-[28px] leading-[28px]`}>
+          <h2
+            className={`${fraunces.className} text-[28px] leading-[28px] md:text-[32px] md:leading-[48px]`}
+          >
             Why choose us?
           </h2>
-          <p className="text-[15px] leading-[25px] opacity-80">
+          <p className="text-[15px] leading-[25px] opacity-80 md:pr-[1px]">
             A large part of our role is choosing which particular coffees will
             be featured in our range. This means working closely with the best
             coffee growers to give you a more impactful experience on every
--- app/components/Choose/ChooseItem.tsx
+++ app/components/Choose/ChooseItem.tsx
@@ -13,16 +13,22 @@ export default function ChooseItem({
   imageUrl,
 }: ChooseItemProps) {
   return (
-    <div className="flex h-[382px] w-[279px] flex-col items-center rounded-[8px] bg-cyan px-[12px] pb-[51px] pt-[72px] text-cream">
-      <Image src={imageUrl} alt="" className="mb-[56px] h-[72px] w-[72px]" />
-      <h3
-        className={`${fraunces.className} mb-[24px] text-center text-[24px] leading-[32px]`}
-      >
-        {title}
-      </h3>
-      <p className="max-w-[212px] text-center text-[15px] leading-[25px]">
-        {description}
-      </p>
+    <div className="flex h-[382px] w-[279px] flex-col items-center rounded-[8px] bg-cyan px-[12px] pb-[51px] pt-[72px] text-cream md:h-[180px] md:w-[573px] md:flex-row md:gap-[55px] md:py-[41px] md:pl-[70px] md:pr-[48px]">
+      <Image
+        src={imageUrl}
+        alt=""
+        className="mb-[56px] h-[72px] w-[72px] items-center md:mb-0 md:h-[56px] md:w-[56px]"
+      />
+      <div className="flex flex-col gap-[24px] md:gap-[16px]">
+        <h3
+          className={`${fraunces.className} text-center text-[24px] leading-[32px] md:text-left`}
+        >
+          {title}
+        </h3>
+        <p className="max-w-[212px] text-center text-[15px] leading-[25px] md:max-w-none md:text-left">
+          {description}
+        </p>
+      </div>
     </div>
   );
 }
