6fa89301636ffd75592f01c138ced1a5c9cbafed
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-10-15 20:52:51 +0200

    Feat: Plan Work component UI tablet && desktop

--- app/components/Works/Works.tsx
+++ app/components/Works/Works.tsx
@@ -11,7 +11,7 @@ interface WorksProps {
 export default function Works({ mode }: WorksProps) {
   return (
     <article
-      className={`${mode === 'light' ? 'bg-cream' : 'min-h-[1223px] bg-chooseBg px-[24px] py-[80px]'} mb-[120px] flex w-[327px] flex-col items-center md:mb-[144px] md:w-[689px] md:items-start xl:mb-[200px] xl:w-[1045px]`}
+      className={`${mode === 'light' ? 'bg-cream' : 'min-h-[1223px] bg-chooseBg px-[24px] py-[80px] md:min-h-[622px] md:w-[768px] md:px-[40px] md:pt-[97px] xl:min-h-[653px] xl:w-[1280px] xl:py-[100px] xl:pl-[85px]'} mb-[120px] flex flex-col items-center rounded-[10px] md:mb-[144px] md:w-[689px] md:items-start xl:mb-[200px] xl:w-[1045px]`}
     >
       <h2
         className={`${fraunces.className} ${mode === 'dark' && 'hidden'} mb-[80px] text-center text-[24px] leading-[32px] text-greay md:mb-[40px] md:text-start xl:mb-[80px]`}
